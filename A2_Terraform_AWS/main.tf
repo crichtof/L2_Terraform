89bbb4caddc5f26265d69ea07d882dec916f4d95
crichtof <flynnbinkcs01@gmail.com>
2024-03-14 14:48:46 -0400

    Update main.tf

--- A2_Terraform_AWS/main.tf
+++ A2_Terraform_AWS/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "C:/Users/Flynn/Desktop/A2_Terraform_AWS/vpc"
+  source = "./A2_Terraform_AWS/vpc"
 }
 
 module "ec2" {
-  source = "C:/Users/Flynn/Desktop/A2_Terraform_AWS/ec2"
+  source = "./A2_Terraform_AWS/ec2"
 }
 
 module "security_group" {
-  source = "C:/Users/Flynn/Desktop/A2_Terraform_AWS/security_group"
+  source = "./A2_Terraform_AWS/security_group"
 }
